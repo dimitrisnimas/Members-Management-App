9c14d3ea10bcd37346e8fe8bbc54f193ebc78a40
Dimitris Nimas <hello@dimitrisnimas.gr>
2026-01-05 05:16:53 +0200

    Add Headers

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,20 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'http://localhost:5173',
+            'http://localhost:4173',
+            process.env.FRONTEND_URL,
+            'https://kubikmembersmanagement.netlify.app'
+        ].filter(Boolean);
+
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 app.use(morgan('dev'));
