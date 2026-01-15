014373846cf2f5fd415d3a4919ef5714e20df78f
Dimitris Nimas <hello@dimitrisnimas.gr>
2026-01-15 13:31:49 +0200

    Add tools in superadmin

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -7,11 +7,17 @@ router.use(authenticateToken);
 
 router.get('/', requireRole(['superadmin']), userController.getAllUsers);
 router.get('/expiring', requireRole(['superadmin']), userController.getExpiringMembers);
+router.get('/find-duplicates', requireRole(['superadmin']), userController.findDuplicates);
 router.get('/:id', userController.getUserById);
 router.get('/:id/history', requireRole(['superadmin']), userController.getUserHistory);
+router.post('/', requireRole(['superadmin']), userController.createMember);
 router.post('/:id/approve', requireRole(['superadmin']), userController.approveUser);
 router.post('/:id/deny', requireRole(['superadmin']), userController.denyUser);
 router.put('/:id', requireRole(['superadmin']), userController.updateUser);
+router.patch('/:id/role', requireRole(['superadmin']), userController.changeRole);
 router.delete('/:id', requireRole(['superadmin']), userController.deleteUser);
 
 module.exports = router;
+
+
+
